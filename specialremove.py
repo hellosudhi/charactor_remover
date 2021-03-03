9a53a0fb8c9ad16f700b03efa3a84457bbf480ee
hellosudhii <hellosudhii@gmail.com>
2021-03-03 21:11:55 +0530

    updated

--- specialremove.py
+++ specialremove.py
@@ -2,26 +2,26 @@
 import csv
 import string
 import os
+import io
 def main():
     filename = "hi.csv"
     output_file = "out.csv"
     reader(filename,output_file)
 
 def reader(filename,output_file):
-    with open (filename,'r',encoding="utf-8") as csvfile:
+    with io.open (filename,'r',encoding="utf8") as csvfile:
         writers(csvfile,output_file)
 
 def writers(csvfile,output_file):
     # add specail charactor
-    remove_bar =  ["#","$"] 
+    remove_bar =  ["#","$","[]"] 
     line = ''
-    with open (output_file,"w",newline = "") as ofile:
+    with io.open (output_file,"w",encoding="utf8") as ofile:
         newf = csv.writer(ofile)
         for x in csvfile:
             line = str(x)
             for sp in remove_bar:
                 line = str.replace(line,sp,'')
-            print("output"+line)
             newf.writerow(line.split(','))
  
 if __name__=="__main__":
